ca2e92bbab1a4c64e6ed97bf55464bf7bd7fb587
henry-sanchez <73953834+henry-sanchez@users.noreply.github.com>
2022-08-25 21:22:50 -0400

    modificando el archivo demo.js

--- demo.js
+++ demo.js
@@ -8,3 +8,6 @@ console.log('Diplomado Fullstack');
 
 console.log('hola clase dia 6 - 25/08/2022');
 console.log('Diplomado Fullstack');
+
+console.log("Buenos noches clase dia 6 - 25/08/2022");
+console.log("Diplomado Fullstack");
