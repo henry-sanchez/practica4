7dd753885611af5f50eb594ce1498e424efae3d2
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-08-25 20:49:48 -0400

    agregando fecha 25/08/2022 al archivo demo

--- demo.js
+++ demo.js
@@ -5,3 +5,6 @@ console.log('Diplomado Fullstack');
 
 console.log('hola clase dia 6');
 console.log('Diplomado Fullstack');
+
+console.log('hola clase dia 6 - 25/08/2022');
+console.log('Diplomado Fullstack');
